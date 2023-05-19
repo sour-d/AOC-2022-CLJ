81477db6ae2b0f7ab8a6190bb16bfa2145cd60e5
sour-d <36912347+sour-d@users.noreply.github.com>
2023-05-19 14:19:30 +0530

    solved day1

---
+++ day1/solution.clj
@@ -0,0 +1,20 @@
+(ns solution
+  (:require [clojure.string :as cstr]))
+
+(def input (slurp "./day1/input-big.txt"))
+
+(->> (cstr/split input #"\n")
+     (partition-by empty?)
+     (take-nth 2)
+     (map #(map read-string %))
+     (map #(apply + %))
+     (apply max))
+
+(->> (cstr/split input #"\n")
+     (partition-by empty?)
+     (take-nth 2)
+     (map #(map read-string %))
+     (map #(apply + %))
+     (sort #(- %2 %1))
+     (take 3)
+     (apply +))
